e705884d354bd870a268c7435bfe495b57bf4abb
ChalleStr <chsr19@LAPTOP-OQQFOB7Q.localdomain>
2020-11-04 16:12:55 +0100

    Updated .gitignore.

---
+++ build/precache-manifest.e46640248d17f586e29dbae0d4109ef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f562846e696b9dfbe64dfc5ecb3958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccca9c8d8239734d3cbd",
+    "url": "/static/css/main.ffae2dde.chunk.css"
+  },
+  {
+    "revision": "93926a5316d6eae7c26f",
+    "url": "/static/js/2.dc842ea1.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.dc842ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccca9c8d8239734d3cbd",
+    "url": "/static/js/main.95c349f9.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "664e77e5b9b4b22597ccf8e88831eb31",
+    "url": "/static/media/charlottestrand.664e77e5.jpg"
+  }
+]);
